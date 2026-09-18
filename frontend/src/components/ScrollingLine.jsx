1cef44cb84306236091ad8819b91f302889ee17e
imsk18 <kumarshatrudhan94263@gmail.com>
2026-09-19 01:57:08 +0530

    scrolling line fixed

--- frontend/src/components/ScrollingLine.jsx
+++ frontend/src/components/ScrollingLine.jsx
@@ -1,172 +1,112 @@
-// import { useEffect } from 'react';
-// import gsap from 'gsap';
-// import '../styles/ScrollingLine.css';
 
-// const items = Array.from({ length: 6 }, (_, index) => index + 1);
-// const imageUrl = 'https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg';
 
-// function ScrollingLine() {
-//   useEffect(() => {
-//     const wheelHandler = (event) => {
-//       if (event.deltaY > 0) {
-//         gsap.to('.marquee', {
-//           transform: 'translateX(-200%)',
-//           repeat: -1,
-//           duration: 4,
-//           ease: 'linear'
-//         });
 
-//         gsap.to('.marquee1', {
-//           transform: 'translateX(-200%)',
-//           repeat: -1,
-//           duration: 4,
-//           ease: 'linear'
-//         });
 
-//         gsap.to('.Arrow, .marquee1 img', {
-//           rotate: 180
-//         });
-//       } else {
-//         gsap.to('.marquee', {
-//           transform: 'translateX(0%)',
-//           repeat: -1,
-//           duration: 4,
-//           ease: 'linear'
-//         });
 
-//         gsap.to('.marquee1', {
-//           transform: 'translateX(0%)',
-//           repeat: -1,
-//           duration: 4,
-//           ease: 'linear'
-//         });
-
-//         gsap.to('.marquee img, .marquee1 img', {
-//           rotate: 0
-//         });
-//       }
-//     };
-
-//     window.addEventListener('wheel', wheelHandler);
-//     return () => {
-//       window.removeEventListener('wheel', wheelHandler);
-//     };
-//   }, []);
-
-//   return (
-//     <div className="scrolling-line">
-     
-//         <div className="move">
-//           {items.map((item) => (
-//             <div className="marquee" key={`marquee-${item}`}>
-              
-//               <h1>welcome my website</h1>
-//               <img className='Arrow' src={imageUrl} alt="arrow" />
-//               <small>mernstack</small>
-//               <img src="../public/skill-icons/React.svg" alt="" />
-//                <img src="../public/skill-icons/javaScript.svg" alt="" />
-//                <img src="../public/skill-icons/express.svg" alt="" />
-//                 <img src="../public/skill-icons/MongoDb.svg" alt="" />
-//             </div>
-//           ))}
-//         </div>
-
-//         {/* <div className="move1">
-//           {items.map((item) => (
-//             <div className="marquee1" key={`marquee1-${item}`}>
-//               <h1>welcome my website</h1>
-//               <img src={imageUrl} alt="arrow" />
-//             </div>
-//           ))}
-//         </div> */}
-//       </div>
-    
-//   );
-// }
-
-// export default ScrollingLine;
-
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import "../styles/ScrollingLine.css";
-
-const items = Array.from({ length: 6 }, (_, index) => index + 1);
 
 const imageUrl =
   "https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg";
 
 function ScrollingLine() {
-   useEffect(() => {
+  const trackRef = useRef(null);
+
+  useEffect(() => {
+    const track = trackRef.current;
+    const arrows = track.querySelectorAll(".Arrow");
+    let direction = 1;
+    let position = 0;
+    let groupWidth = track.firstElementChild.offsetWidth;
+
+    const updatePosition = () => {
+      position -= direction * 1.2 * gsap.ticker.deltaRatio(60);
+
+      if (position <= -groupWidth) {
+        position += groupWidth;
+      } else if (position >= 0) {
+        position -= groupWidth;
+      }
+
+      gsap.set(track, { x: position });
+    };
+    const resizeHandler = () => {
+      groupWidth = track.firstElementChild.offsetWidth;
+    };
+    const setDirection = (value) => {
+      direction = value > 0 ? 1 : -1;
+      gsap.to(arrows, {
+        rotate: direction === 1 ? 180 : 0,
+        duration: 0.35,
+        overwrite: true,
+      });
+    };
     const wheelHandler = (event) => {
-      if (event.deltaY > 0) {
-        gsap.to('.marquee', {
-          transform: 'translateX(-200%)',
-          repeat: -1,
-          duration: 4,
-          ease: 'linear'
-        });
-
-        gsap.to('.marquee1', {
-          transform: 'translateX(-200%)',
-          repeat: -1,
-          duration: 4,
-          ease: 'linear'
-        });
-
-        gsap.to('.Arrow, .marquee1 img', {
-          rotate: 180
-        });
-      } else {
-        gsap.to('.marquee', {
-          transform: 'translateX(0%)',
-          repeat: -1,
-          duration: 4,
-          ease: 'linear'
-        });
-
-        gsap.to('.marquee1', {
-          transform: 'translateX(0%)',
-          repeat: -1,
-          duration: 4,
-          ease: 'linear'
-        });
-
-        gsap.to('.marquee img, .marquee1 img', {
-          rotate: 0
-        });
+      if (event.deltaY !== 0) setDirection(event.deltaY);
+    };
+    let touchStartX = 0;
+    const touchStartHandler = (event) => {
+      touchStartX = event.touches[0].clientX;
+    };
+    const touchMoveHandler = (event) => {
+      const distance = event.touches[0].clientX - touchStartX;
+      if (Math.abs(distance) > 3) {
+        setDirection(-distance);
+        touchStartX = event.touches[0].clientX;
       }
     };
 
-    window.addEventListener('wheel', wheelHandler);
+    window.addEventListener("wheel", wheelHandler, { passive: true });
+    window.addEventListener("resize", resizeHandler);
+    track.addEventListener("touchstart", touchStartHandler, { passive: true });
+    track.addEventListener("touchmove", touchMoveHandler, { passive: true });
+    gsap.ticker.add(updatePosition);
+
     return () => {
-      window.removeEventListener('wheel', wheelHandler);
+      window.removeEventListener("wheel", wheelHandler);
+      window.removeEventListener("resize", resizeHandler);
+      track.removeEventListener("touchstart", touchStartHandler);
+      track.removeEventListener("touchmove", touchMoveHandler);
+      gsap.ticker.remove(updatePosition);
     };
   }, []);
 
+  const content = Array.from({ length: 6 }, (_, index) => (
+    <div className="marquee" key={index}>
+      <img className="Arrow" src={imageUrl} alt="" />
+      <img
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg"
+        alt="MongoDB"
+      />
+      <img
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+        alt="Express"
+      />
+      <img src="/skill-icons/React.svg" alt="React" />
+      <img
+        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+        alt="Node.js"
+      />
+    </div>
+  ));
+
   return (
-    <div className="scrolling-line">
-      <div className="move">
-        {items.map((item) => (
-          <div className="marquee" key={`marquee-${item}`}>
-            {/* <h1>welcome my website</h1> */}
-
-            <img className="Arrow" src={imageUrl} alt="arrow" />
-
-            <small></small>
-
-         
-           
-           
-
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt="" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="" />
-            <img src="/skill-icons/React.svg" alt="" />
-            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt="" />
-          </div>
-        ))}
+    <div className="scrolling-line" style={{backgroundColor:"red"}}>
+      <div className="move" ref={trackRef} >
+        <div className="marquee-group" >{content} </div>
+        <div className="marquee-group" aria-hidden="true"style={{width:"100%"}}  >
+          {content}
+        </div>
       </div>
     </div>
   );
 }
 
 export default ScrollingLine;
+
+
+
+
+
+
